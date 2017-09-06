f47cfa1a0c7b8f02499313091f9c146c1face4c9
Unknown <domagalski.dominik@gmail.com>
2017-09-06 23:09:26 +0200

    v1-beta_fixed3

--- brstub.cs
+++ brstub.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Security.Cryptography;
 using System.IO;
 using System.Text;
@@ -12,9 +12,10 @@ public class brstub
     //variables
 
     byte[] decrypted;
-    public byte[] Decrypted { get => decrypted };
 
-    enum regKeyType
+    public byte[] Decrypted { get => decrypted; }
+
+    public enum regKeyType
     {
         CurrentUser,
         LocalMachine
@@ -45,6 +46,29 @@ public class brstub
 
     //PUBLIC METHODS
 
+    //untested
+    public static string GetMd5Hash(string input)
+    {
+
+        
+        MD5 md5Hash = MD5.Create(); 
+        // Convert the input string to a byte array and compute the hash.
+        byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+        // Create a new Stringbuilder to collect the bytes
+        // and create a string.
+        StringBuilder sBuilder = new StringBuilder();
+
+        // Loop through each byte of the hashed data 
+        // and format each one as a hexadecimal string.
+        for (int i = 0; i < data.Length; i++)
+        {
+            sBuilder.Append(data[i].ToString("x2"));
+        }
+
+        // Return the hexadecimal string.
+        return sBuilder.ToString();
+    }
 
     /// <summary>
     /// Encrypt file with random key and IV.
